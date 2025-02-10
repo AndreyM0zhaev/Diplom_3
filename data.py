8ac0778ba9325f9ae8de082894402f1dc578803e
Andrey Mozhaev <Mozhaev.andrey.v@yandex.com>
2025-02-10 21:23:43 +0300

    func: add data.py

---
+++ data.py
@@ -0,0 +1,11 @@
+NAME = 'Andrey'
+EMAIL = 'Mozhaev.Andrey.v@yandex.ru'
+PASSWORD = 'asdfgh123456!'
+
+BASE_URL = 'https://stellarburgers.nomoreparties.site/'
+LOGIN_URL = 'https://stellarburgers.nomoreparties.site/login'
+PROFILE_URL = 'https://stellarburgers.nomoreparties.site/account/profile'
+REGISTER_URL = 'https://stellarburgers.nomoreparties.site/register'
+PASSWORD_RECOVERY_URL = "https://stellarburgers.nomoreparties.site/forgot-password"
+ORDERS_LIST_URL = "https://stellarburgers.nomoreparties.site/feed"
+
