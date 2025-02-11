bfb8a1fa668c48db60c78485e1f66be34d471b09
Andrey Mozhaev <Mozhaev.andrey.v@yandex.com>
2025-02-11 22:22:04 +0300

    func: add user_account_locators

---
+++ locators/user_account_locators.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.common.by import By
+
+
+class UserAccountLocators:
+    # Локатор кнопки "Личный Кабинет"
+    BUTTON_USER_ACCOUNT = [By.XPATH, ".//p[text()='Личный Кабинет']"]
+
+    # Локатор текста "Профиль"
+    TEXT_PROFILE = [By.XPATH, ".//a[text()='Профиль']"]
+
+    # Локатор кнопки "История заказов"
+    BUTTON_ORDERS_HISTORY = [By.XPATH, ".//a[text()='История заказов']"]
+
+    # Локатор текста заказа "Флюоресцентный бургер"
+    TEXT_ORDER = [By.XPATH, ".//h2[text()='Флюоресцентный бургер']"]
+
+    # Локатор кнопки "Выход"
+    BUTTON_EXIT = [By.XPATH, ".//button[text()='Выход']"]
